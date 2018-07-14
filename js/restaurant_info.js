64ba4893baf3eaf09a4c69262c53822dc0701a85
Ed Costa <edcosta94@gmail.com>
2018-07-13 23:51:35 -0300

    Restaurant Review
    to run this project, open you cmd and type:
    
    python -m http.server
    
    or
    
    python3 -m http.server
    
    and access the page: http://localhost:8000 in your favorite browser.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -21,8 +21,8 @@ initMap = () => {
         zoom: 16,
         scrollWheelZoom: false
       });
-      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg?access_token={mapboxToken}', {
+        mapboxToken: 'pk.eyJ1IjoiZWRzbWNvc3RhIiwiYSI6ImNqamFycmJ3ZTNrbGgzcW1tNjQ3ZjZvbXUifQ.0uWMXTT2unk4jhtASZXUmQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
